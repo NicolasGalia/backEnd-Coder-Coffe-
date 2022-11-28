1b81a63f6ba4c641d8f539c3a79a7da1a547241b
EduRossB <edu_barba@outlook.com>
2022-11-28 08:01:00 -0300

    borre rutas innecesarias

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -39,38 +39,4 @@ router
     generarPedidos
   );
 
-router
-  .route("/pedido")
-  .post(
-    [
-      check("producto", "El pedido debe tener productos").notEmpty(),
-      check("usuarioId", "El pedido debe tener un comprador").notEmpty(),
-    ],
-    generarPedido
-  )
-  .put(
-    [
-      check("productos", "El pedido debe tener productos").notEmpty(),
-      check("usuarioId", "El pedido debe tener un comprador").notEmpty(),
-      check("total", "El total del pedido es obligatorio")
-        .notEmpty()
-        .isNumeric()
-        .withMessage("El total debe ser numerico")
-        .custom((value) => {
-          if (value >= 1 && value <= 100000) {
-            return true;
-          } else {
-            throw new Error("El total debe estar entre 1 y 100000");
-          }
-        }),
-      check("idPedido", "El id del pedido es obligatorio").notEmpty(),
-      check("estado", "El estado del pedido es obligatorio")
-        .notEmpty()
-        .isIn(["pendiente", "entregado"])
-        .withMessage("El estado del pedido no es valido (pendiente/entregado)"),
-    ],
-    editarPedido
-  )
-  .delete(limpiarPedido);
 
-export default router;
