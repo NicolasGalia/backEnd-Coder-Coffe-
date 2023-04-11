580de84017d31cb07c5e9ff232fb61d22e467e09
Nicolas GaliaM <nicolasgaliam@gmail.com>
2023-04-11 20:47:16 -0300

    configuracion de mongo

--- src/database/database.js
+++ src/database/database.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 
 const url = process.env.MONGO_CONNECTION_URL;
 
-mongoose.connect(url);
+mongoose.connect(url, {
+    useUnifiedTopology: true   
+});
 
 const connection = mongoose.connection;
 
-connection.once('open', ()=>{
-    
-})
+connection.once('open', () => {})
