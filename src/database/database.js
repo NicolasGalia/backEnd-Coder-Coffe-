b0d79b6fc931075347946fdc314f3d5646c8c438
Nicolas GaliaM <nicolasgaliam@gmail.com>
2023-04-11 20:13:41 -0300

    agrego la url de la db desde .env

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 import mongoose from "mongoose";
 
-// const url = `${process.env.MONGO_CONNECTION_URL}/${process.env.DATA_BASE_NAME}`;
-const url = 'mongodb+srv://nicolasgaliam:nico12345@cluster0.7hzvgkb.mongodb.net/coder-coffe'
+const url = process.env.MONGO_CONNECTION_URL;
+
 mongoose.connect(url);
 
 const connection = mongoose.connection;
